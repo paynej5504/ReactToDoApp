54c4bb61da3d15fddf0db510178ba981d475dc89
paynej5504 <54283576+paynej5504@users.noreply.github.com>
2020-12-05 15:31:56 -0500

    Add files via upload

---
+++ src/components/InputTodo.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+class InputTodo extends Component {
+    state = {
+        title: ""
+    };
+
+    onChange = e => {
+        this.setState({
+            title: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addTodoProps(this.state.title);
+        this.setState({ title: ""
+        });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="form-container">
+                <input type="text" className="input-text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}/>
+                <input type="submit" className="input-submit" value="Submit" />
+            </form>
+        );
+    }
+}
+export default InputTodo;
